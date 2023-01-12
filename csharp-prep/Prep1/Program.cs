d60e7745c3260f4413ad964d1aef96a044f0f70e
mmusih <hub22001@byui.edu>
2023-01-12 12:23:10 +0200

    week111

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first Name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string surname = Console.ReadLine();
+        Console.WriteLine(" ");
+        //Console.Write("Your name is ");
+        //Console.Write(surname);
+        //Console.Write(", ");
+        //Console.Write(name);
+        //Console.Write(" ");
+        //Console.Write(surname);
+        //Console.WriteLine(".");
+        Console.WriteLine($"Your name is {surname}, {name} {surname}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your Percentage? ");
+        string userInput = Console.ReadLine();
+        int score = int.Parse(userInput);
+        
+        string letter = "";
+
+        if (score >= 90)
+        {
+            letter = "A";
+        }
+        else if (score >= 80)
+        {
+            letter = "B";
+        }
+        else if (score >= 70)
+        {
+            letter = "C";
+        }
+        else if (score >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Grade: {letter}");
+            if (score >= 70)
+            {
+                Console.WriteLine("Congratulations you have PASSED!");
+            }
+            else
+            {
+                Console.WriteLine("You have failed the exam, good luck on your next attempt.");
+            }
+
     }
 }
