b878796ff97587fffbcf04faec8b24deb060054b
mmusih <hub22001@byui.edu>
2023-01-14 16:46:13 +0200

    week2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 11);
+        Console.Write("What is the magic number? ");
+
+        int guess = -1;
+
+        while (guess != number)
+        {
+            Console.Write("What is your Guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it.");
+            }
+        }
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int number = -1;
+        while (number != 0)
+        {
+            Console.Write("Enter a number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
+
+        Console.WriteLine(numbers.Count);
+
+        int sum = 0;
+        foreach (int usernumber in numbers)
+        {
+            sum += usernumber;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        float average = ((float)sum / numbers.Count);
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+        foreach (int bigNumber in numbers)
+        {
+            if (bigNumber > max)
+            {
+                max = bigNumber;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
